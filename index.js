de0e4440a105df588823b6b4afca53c1a062d64e
Henrique Lira <henriquelira.sp@gmail.com>
2026-04-15 14:59:29 -0300

    Ajusta visibilidade e responsividade dos requisitos de senha

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const userInputs = {}
 userInputs.name = document.querySelector('#name')
 userInputs.email = document.querySelector("#email")
 userInputs.password = document.querySelector('#password')
+userInputs.confirmPassword = document.querySelector('#confirmPassword')
+
+const reqLength = document.getElementById("req-length")
+const reqUppercase = document.getElementById("req-uppercase")
+const reqLowercase = document.getElementById('req-lowercase')
+const reqNumber = document.getElementById("req-number")
+const reqSpecial = document.getElementById("req-special")
 
 const rules = {
     name: /^[A-Za-zÀ-ÿ\s]{2,}$/,
@@ -52,6 +59,15 @@ function validatePassword(password) {
     }
 }
 
+function confirmPassword() {
+
+    if(userInputs.confirmPassword.value !== userInputs.password.value) {
+        const err = new Error('As senhas não conferem.')
+        err.input = 'confirmPassword'
+        throw err
+    }
+}
+
 function removeFromStyles() {
     Object.entries(userInputs).forEach(([key, value]) => {
         value.classList.remove('success', 'error')
@@ -63,7 +79,8 @@ function createAccount () {
     const validations = {
             name: validateName,
             email: validateEmail,
-            password: validatePassword
+            password: validatePassword,
+            confirmPassword : confirmPassword
         }
 
         Object.entries(validations).forEach(([key, validate]) => {
@@ -92,8 +109,43 @@ form.addEventListener('submit', (ev) => {
 
 })
 
+const div = document.querySelector('#password-requirements-container')
+
+//mostra os requisitos da senha ao clicar no campo
 userInputs.password.addEventListener('focus', () => {
-    const div = document.querySelector('#password-requirements-container')
-    div.style.display = 'block'
+    div.classList.add('active')
 })
+
+//esconde os requisitos da senha ao sair do campo
+userInputs.password.addEventListener('blur', () => {
+    div.classList.remove('active')
+})
+
+//atualiza e remove a mensagem de error enquanto o usuario digita a senha
+userInputs.password.addEventListener('input', () => {
+    updateRequeriment(reqLength, userInputs.password.value.length >= 8)
+    updateRequeriment(reqUppercase, /[A-Z]/.test(userInputs.password.value))
+    updateRequeriment(reqLowercase, /[a-z]/.test(userInputs.password.value))
+    updateRequeriment(reqNumber, /[0-9]/.test(userInputs.password.value))
+    updateRequeriment(reqSpecial, /[!@#$%^&*(),.?\":{}|<>]/.test(userInputs.password.value))
+
+
+    const err = document.querySelector('#password-error')
+    err.textContent = ''
+    userInputs.password.classList.remove('error')
+})
+
+userInputs.confirmPassword.addEventListener('input', () => {
+    const err = document.querySelector('#confirmPassword-error')
+    err.textContent = ''
+    userInputs.confirmPassword.classList.remove('error')
+})
+
+function updateRequeriment(element, isValid) {
+    if (isValid) {
+        element.classList.add('valid')
+    } else {
+        element.classList.remove('valid')
+    }
+}
 
